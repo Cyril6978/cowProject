ce369dfc0e249cba37f7df3155cca63ad53b3ace
Cyril6978 <cyril.david8978@gmail.com>
2022-12-05 15:49:41 +0100

    CreateCow

---
+++ index.js
@@ -0,0 +1,10 @@
+const userInfo = require('./information');
+
+let cowsay = require("cowsay");
+
+console.log(cowsay.say({
+    text : `Hello, i'm ${userInfo.name} From ${userInfo.campus}`,
+    e : "oO",
+    T : "U "
+}));
+// or cowsay.think()
---
+++ information.js
@@ -0,0 +1,9 @@
+const moi = {
+    name: "cyril",
+    campus: "wild",
+};
+
+module.exports = {
+    name: moi.name,
+    campus: moi.campus,
+};
